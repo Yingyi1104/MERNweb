9dd51060c0376fa890847e16674ea6d41f7ba5f7
BigBro1104 <wuyingyi1104@gmail.com>
2019-04-20 20:49:25 -0400

    finish user API routers and validation handlers

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://Yingyi:Wuyingyi1994!@ds119164.mlab.com:19164/sdproject'
+  mongoURI: 'mongodb://Yingyi:Wuyingyi1994!@ds119164.mlab.com:19164/sdproject',
+  secretOrKey:'secret'
 };
 
--- server.js
+++ server.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const passport = require('passport');
 
 const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //DB Config
 const db = require('./config/keys').mongoURI;
 
 //Connect to MongoDB
-mongoose.connect(db).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
+mongoose.connect(db, {useNewUrlParser:true}, function(err){
+　　if(err){
+　　　　console.log('Connection Error:' + err)
+　　}else{
+　　　　console.log('Connection success!')
+　　}
+})
 
 
-app.get('/', (req, res) => res.send('Hello'));
+//passport middleware
+app.use(passport.initialize());
+
+//passport config
+require('./config/passport')(passport);
 
 //Use Routes
 app.use('/api/users', users);
