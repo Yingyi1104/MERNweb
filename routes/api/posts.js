b9e4b3099949281af55609a9e2762edbb53f9bab
BigBro1104 <wuyingyi1104@gmail.com>
2019-04-26 10:16:35 -0400

    clean my code

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -12,12 +12,10 @@ const Profile = require('../../models/Profile');
 const validatePostInput = require('../../validation/post');
 
 // @route   GET api/posts/test
-// @desc    Tests post route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
 
 // @route   GET api/posts
-// @desc    Get posts
 // @access  Public
 router.get('/', (req, res) => {
   Post.find()
@@ -27,7 +25,6 @@ router.get('/', (req, res) => {
 });
 
 // @route   GET api/posts/:id
-// @desc    Get post by id
 // @access  Public
 router.get('/:id', (req, res) => {
   Post.findById(req.params.id)
@@ -38,7 +35,6 @@ router.get('/:id', (req, res) => {
 });
 
 // @route   POST api/posts
-// @desc    Create post
 // @access  Private
 router.post(
   '/',
@@ -64,7 +60,6 @@ router.post(
 );
 
 // @route   DELETE api/posts/:id
-// @desc    Delete post
 // @access  Private
 router.delete(
   '/:id',
@@ -89,7 +84,6 @@ router.delete(
 );
 
 // @route   POST api/posts/like/:id
-// @desc    Like post
 // @access  Private
 router.post(
   '/like/:id',
@@ -118,7 +112,6 @@ router.post(
 );
 
 // @route   POST api/posts/unlike/:id
-// @desc    Unlike post
 // @access  Private
 router.post(
   '/unlike/:id',
@@ -153,7 +146,6 @@ router.post(
 );
 
 // @route   POST api/posts/comment/:id
-// @desc    Add comment to post
 // @access  Private
 router.post(
   '/comment/:id',
@@ -187,7 +179,6 @@ router.post(
 );
 
 // @route   DELETE api/posts/comment/:id/:comment_id
-// @desc    Remove comment from post
 // @access  Private
 router.delete(
   '/comment/:id/:comment_id',
--- server.js
+++ server.js
@@ -1,38 +1,42 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const passport = require('passport');
 
 const app = express();
 
-// Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-// DB Config
+//DB Config
 const db = require('./config/keys').mongoURI;
 
-// Connect to MongoDB
-mongoose
-  .connect(db)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+//Connect to MongoDB
+mongoose.connect(db, {useNewUrlParser:true}, function(err){
+　　if(err){
+　　　　console.log('Connection Error:' + err)
+　　}else{
+　　　　console.log('Connection success!')
+　　}
+})
 
-// Passport middleware
+
+//passport middleware
 app.use(passport.initialize());
 
-// Passport Config
+//passport config
 require('./config/passport')(passport);
 
-// Use Routes
+//Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, ()=> console.log("Server running on port %d",port));
